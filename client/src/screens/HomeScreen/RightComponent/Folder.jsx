8d87e29b6cd0ec075834bbd84541187feed731d2
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-05-22 20:34:33 +0530

    create playground ID with uuid

--- client/src/screens/HomeScreen/RightComponent/Folder.jsx
+++ client/src/screens/HomeScreen/RightComponent/Folder.jsx
@@ -4,23 +4,26 @@ import { PlaygroundContext } from "../../../Providers/PlaygroundProvider";
 import { modalConstants, ModalContext } from "../../../Providers/ModalProvider";
 
 export const Folder = ({ folderTitle, cards, folderId }) => {
-
   const playgroundFeatures = useContext(PlaygroundContext);
-  const {openModal, setModalPayload} = useContext(ModalContext);
+  const { openModal, setModalPayload } = useContext(ModalContext);
 
   const handleDeleteFolder = () => {
     playgroundFeatures.deleteFolder(folderId);
-  }
+  };
 
   const onEditFolderTitle = () => {
     setModalPayload(folderId);
     openModal(modalConstants.UPDATE_FOLDER_TITLE);
-  }
+  };
 
   const openCreateCardModal = () => {
-    setModalPayload(folderId);
+    const playgroundID = uuidv4();
+    setModalPayload({
+      folderId,
+      playgroundID,
+    });
     openModal(modalConstants.CREATE_CARD);
-  }
+  };
 
   return (
     <div className="folder-container">
@@ -33,9 +36,13 @@ export const Folder = ({ folderTitle, cards, folderId }) => {
         </div>
 
         <div className="folder-header-item">
-          <span className="material-icons" onClick={handleDeleteFolder}>delete</span>
-          <span className="material-icons" onClick={onEditFolderTitle}>edit</span>
-          <button style={{cursor:'pointer'}} onClick={openCreateCardModal}>
+          <span className="material-icons" onClick={handleDeleteFolder}>
+            delete
+          </span>
+          <span className="material-icons" onClick={onEditFolderTitle}>
+            edit
+          </span>
+          <button style={{ cursor: "pointer" }} onClick={openCreateCardModal}>
             <span className="material-icons">add</span>
             <span>New Playground</span>
           </button>
--- client/src/screens/HomeScreen/index.jsx
+++ client/src/screens/HomeScreen/index.jsx
@@ -1,31 +1,45 @@
-import { useContext } from "react"
-import { Modal } from "../../Providers/Modals/Modal"
-import "./index.scss"
-import { RightComponent } from "./RightComponent"
-import { ModalContext } from "../../Providers/ModalProvider"
+import { useContext, useState } from "react";
+import { Modal } from "../../Providers/Modals/Modal";
+import "./index.scss";
+import { RightComponent } from "./RightComponent";
+import { ModalContext } from "../../Providers/ModalProvider";
+import { v4 as uuidv4 } from "uuid";
 
 export const HomeScreen = () => {
   const modalFeatures = useContext(ModalContext);
-  
+
   const openCreatePlaygroundModal = () => {
+    const playgroundID = uuidv4();
+    console.log("in index.js playgroundID:"+playgroundID);
+    modalFeatures.setModalPayload({ playgroundID });
     modalFeatures.openModal("CREATE_PLAYGROUND");
-  }
+  };
+
+  const openJoinPlaygroundModal = () => {};
 
   return (
     <div className="home-container">
       <div className="left-container">
-          <div className="items-container">
-            <img src="/logo.webp" alt="LiveCodeHub Logo" style={{borderRadius:"20%"}}/>
-            <h1>LiveCode Hub</h1>
-            <h3>Code.Compile.Debug</h3>
-            <button onClick={openCreatePlaygroundModal} >
-              <span className="material-icons">add</span>
-              <span>Create Playground</span>
-            </button>
-          </div>
+        <div className="items-container">
+          <img
+            src="/logo.webp"
+            alt="LiveCodeHub Logo"
+            style={{ borderRadius: "20%" }}
+          />
+          <h1>LiveCode Hub</h1>
+          <h3>Code.Compile.Debug</h3>
+          <button onClick={openCreatePlaygroundModal}>
+            <span className="material-icons">add</span>
+            <span>Create Playground</span>
+          </button>
+          <button onClick={openJoinPlaygroundModal}>
+            <span className="material-icons">add</span>
+            <span>Join Playground</span>
+          </button>
+        </div>
       </div>
-      <RightComponent/>
-      <Modal/>
+      <RightComponent />
+      <Modal />
     </div>
-  )
-}
+  );
+};
