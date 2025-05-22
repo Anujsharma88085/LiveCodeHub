1a10b03d0dd35777f2b0b38ed64d03ef18c0bf9c
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-05-22 20:30:49 +0530

    create playground Modal with playground ID

--- client/src/Providers/Modals/CreatePlaygroundModal.jsx
+++ client/src/Providers/Modals/CreatePlaygroundModal.jsx
@@ -1,44 +1,75 @@
 import { useContext } from "react";
 import "./createPlaygroundModal.scss";
 import { ModalContext } from "../ModalProvider";
-import { PlaygroundContext } from "../PlaygroundProvider";
+import { defaultCodes, PlaygroundContext } from "../PlaygroundProvider";
+// import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const CreatePlaygroundModal = () => {
+  // const navigate = useNavigate();
   const ModalFeatures = useContext(ModalContext);
   const playgroundFeatures = useContext(PlaygroundContext);
 
   const closeModal = () => {
     ModalFeatures.closeModal();
-  }
+  };
 
-  const onSubmitModal = (e) => {
+  const onSubmitModal = async (e) => {
     e.preventDefault();
     const folderName = e.target.folderName.value;
     const fileName = e.target.fileName.value;
     const language = e.target.language.value;
+    const playgroundID = ModalFeatures.modalPayload?.playgroundID || "";
     playgroundFeatures.createNewPlayground({
       folderName,
       fileName,
       language,
     });
 
-   closeModal();
-  }
+    try {
+      await axios.post('http://localhost:3000/api/v1/playground/createPlayground', {
+        playgroundID,
+        folderName,
+        fileName,
+        language,
+        code: defaultCodes[language],
+      });
+      // navigate(`/playground/${playgroundID}`);
+    } catch (err) {
+      console.error('Error creating playground:', err);
+    }
+
+    closeModal();
+  };
 
   return (
     <div className="modal-container">
       <form className="modal-body" onSubmit={onSubmitModal}>
-        <span className="material-icons close" onClick={closeModal} style={{cursor:"pointer"}}>close</span>
+        <span
+          className="material-icons close"
+          onClick={closeModal}
+          style={{ cursor: "pointer" }}
+        >
+          close
+        </span>
 
-        <h1>Create New Playground</h1>
         <div className="item">
-          <p>Enter Folder Name</p>
-          <input type="text" name="folderName" required/>
+          <p>Playground ID</p>
+          <input
+            type="text"
+            value={ModalFeatures.modalPayload?.playgroundID || ""}
+            readOnly
+          />
         </div>
 
         <div className="item">
-          <p>Enter Card Name</p>
-          <input type="text" name="fileName" required/>
+          <p>Enter Folder Name</p>
+          <input type="text" name="folderName" required />
+        </div>
+
+        <div className="item">
+          <p>Enter Playground Name</p>
+          <input type="text" name="fileName" required />
         </div>
 
         <div className="item">
@@ -49,11 +80,9 @@ export const CreatePlaygroundModal = () => {
             <option value="python">PYTHON</option>
           </select>
 
-          <button type="submit">
-            Create Playground
-          </button>
+          <button type="submit">Create Playground</button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
